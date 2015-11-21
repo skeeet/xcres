f94919fb37365470aba6474275038005d72f40c2
Ivan Sinkarenko <ivan.sinkarenko@qnective.com>
2015-11-21 12:10:57 +0100

    Better tolerance of IB identifiers

--- lib/xcres/analyzer/strings_analyzer.rb
+++ lib/xcres/analyzer/strings_analyzer.rb
@@ -234,7 +234,7 @@ module XCRes
         strings = read_strings_file(path)
 
         # Reject generated identifiers used by Interface Builder
-        strings.reject! { |key, _| /^[a-zA-Z0-9]{3}(-[a-zA-Z0-9]{3}){2}/.match(key) }
+        strings.reject! { |key, _| /^[a-zA-Z0-9]+(-[a-zA-Z0-9]+){2}/.match(key) }
 
         keys = Hash[strings.map do |key, value|
           [key, XCRes::String.new(key, key, value.gsub(/[\r\n]/, ' '))]
